e36c0f70c86304190f8c0e9bf608d684f66a06b9
Dustin Cintron <D.Cint88@gmail.com>
2018-08-27 16:39:14 -0400

    Cintron tool initial commit

---
+++ lib/cintron_view_tool.rb
@@ -0,0 +1,6 @@
+require "cintron_view_tool/version"
+require "cintron_view_tool/renderer"
+
+module CintronViewTool
+  # Your code goes here...
+end
---
+++ lib/cintron_view_tool/version.rb
@@ -0,0 +1,3 @@
+module CintronViewTool
+  VERSION = "0.1.0"
+end
